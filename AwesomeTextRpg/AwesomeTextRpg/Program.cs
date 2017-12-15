d9bf33104c34b42de8b709c330c14b7ec2bea6c1
Constantinos Ziazios <ziaziosk@gmail.com>
2017-12-15 17:33:14 +0200

    Add Learning Projects (Class lesson)

---
+++ AwesomeTextRpg/AwesomeTextRpg/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeTextRpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The tower of horrors!!!");
+            Hero myHero = new Hero();
+           
+            CreateCharacter(myHero);
+            
+            Console.WriteLine("Hello mighty " + myHero.Name);
+            Console.WriteLine("The tower of horrors is really cold this time of year...");
+            Console.WriteLine("You wake up in your cold cell. Freezing... You feel really sick");
+            Console.WriteLine("The guard enters our cell.");
+            Console.WriteLine("Confess he shouts.");
+            Console.WriteLine("What do you do?");
+            Console.WriteLine("Press 1 to confess. Press 2 to try to break your bonds.");
+            string answer = Console.ReadLine();
+            if (answer == "1")
+            { }
+            else if (answer == "2")
+            {
+                if (myHero.Strength > 18)
+                { }
+                else { }
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+        public static void CreateCharacter(Hero hero )
+        {
+            Console.WriteLine("What's your character's name");
+            hero.Name = Console.ReadLine();
+            Console.WriteLine("What's your character's strength (1-20)");
+            hero.Strength = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's your character's charisma (1-20)");
+            hero.Charisma = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's your character's intelligence (1-20)");
+            hero.Intelligence = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's your character's wisdom (1-20)");
+            hero.Wisdom = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+       
+    }
+    public class Hero
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int HitPoints { get; set; }
+
+        public int Intelligence { get; set; }
+        public int Charisma { get; set; }
+
+        public int Wisdom { get; set; }
+        public int Dexterity { get; set; }
+        public int Defense { get; set; }
+
+
+
+    }
+}
---
+++ FirstClass/FirstClass/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> persons = new List<Person>();
+
+            while (true)
+            {
+                string nameFromUser = Console.ReadLine();
+                if(nameFromUser == "e")
+                {
+                    break;
+                }
+
+                var person = new Person();
+                person.ReadPerson();
+                persons.Add(person);
+            }
+
+
+            foreach(var person in persons)
+            {
+                person.Epidoma();
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public List<Person> Children { get; set; } = new List<Person>();
+
+        public void ReadPerson()
+        {
+            FirstName = Console.ReadLine();
+            LastName = Console.ReadLine();
+            int numberOfChilds = Convert.ToInt32(Console.ReadLine());
+
+            for(var i=0; i < numberOfChilds; i++)
+            {
+                var child = new Person();
+                child.ReadPerson();
+                Children.Add(child);
+            }
+        }
+
+        public void DisplayPerson()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+            foreach(var child in Children)
+                Console.WriteLine($"\t{child.FirstName} {child.LastName}");
+        }
+
+        public void Epidoma()
+        {
+            decimal epidoma = 100m;
+            Console.WriteLine(epidoma * Children.Count);
+        }
+    }
+
+
+}
